f9913757c03111327b9e23488726c7fac8c4b047
Ales Komarek <ales-komarek@seznam.cz>
2024-12-11 17:35:07 +0100

    conf: bigger font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
-static char *font2 = "Roboto Mono for Powerline:pixelsize=13:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font Mono :pixelsize=16:antialias=true:autohint=true";
+static char *font2 = "Roboto Mono for Powerline:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
